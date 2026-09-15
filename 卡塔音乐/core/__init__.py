3fc9e6119d7089b8321ab43f5734b9d311ef111d
kata <3424409635@qq.com>
2026-09-15 14:21:33 +0800

    Delete 卡塔音乐 directory

--- 卡塔音乐/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core framework for multi-platform music downloader."""
-
-from .platform_base import BasePlatform
-from .downloader import DownloadManager
-
-__all__ = ["BasePlatform", "DownloadManager"]
--- 卡塔音乐/platforms/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Music platform integrations."""
-
-from platforms.netease.api import NeteaseAPI
-
-__all__ = ["NeteaseAPI"]
